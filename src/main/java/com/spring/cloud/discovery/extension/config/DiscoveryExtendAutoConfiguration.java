dad325dd6f52c26191c5f02693693fa80a32391e
chenzfus <710239691@qq.com>
2021-12-30 11:45:23 +0800

    [ADD] 新建工程

---
+++ src/main/java/com/spring/cloud/discovery/extension/config/DiscoveryExtendAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.discovery.extension.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author chenzfus 2021/12/30 11:38
+ * @date 2021/12/30 11:38
+ */
+@Configuration
+public class DiscoveryExtendAutoConfiguration {
+}
---
+++ src/main/java/com/spring/cloud/discovery/extension/loadbalance/ExtendLoadBalancer.java
@@ -0,0 +1,9 @@
+package com.spring.cloud.discovery.extension.loadbalance;
+
+/**
+ *
+ * @author chenzfus 2021/12/30 11:38
+ * @date 2021/12/30 11:38
+ */
+public class ExtendLoadBalancer {
+}
